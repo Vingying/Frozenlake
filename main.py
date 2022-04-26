fa98bf5d3f4e9b526fc2d948b1c4211116f3878e
Jingwei Yi <78785339+Vingying@users.noreply.github.com>
2022-04-26 22:06:54 +0800

    first push
    2022 UESTC-RL Homework-Frozenlake

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ import numpy as np
 from gym import Env, spaces, utils
 from gym.envs.toy_text.utils import categorical_sample
 
+
+def random_generate_map(size=8):
+    valid = False
+    def search_path(x, y):
+        
+
+
+    while not valid:
